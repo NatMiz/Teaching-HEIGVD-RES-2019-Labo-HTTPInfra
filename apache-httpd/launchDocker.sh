f5cd30d2687ff65830f4b01edeebda9e9aef8313
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-06-07 23:51:38 +0200

    Updated Dockerfile and removed unsued directory. Modified bootstrap template. Script now build and run containers.

---
+++ apache-httpd/launchDocker.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+APP_NAME="php-app"
+IMAGE_NAME="my-php-app"
+
+docker stop $APP_NAME
+
+docker rm $APP_NAME
+
+docker build -t $IMAGE_NAME .
+
+docker run -d --name $APP_NAME -t $IMAGE_NAME
