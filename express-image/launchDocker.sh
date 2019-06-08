4de41eb16baa81e6a82788bf7f39f4716b5406a4
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-06-08 16:03:14 +0200

    Added command for testing purpose

---
+++ express-image/launchDocker.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+APP_NAME="node-express"
+IMAGE_NAME="res/express-node"
+
+docker stop $APP_NAME
+
+docker rm $APP_NAME
+
+docker build -t $IMAGE_NAME .
+
+#docker run -d --name $APP_NAME -t $IMAGE_NAME
+
+# For testing purpose
+docker run --name $APP_NAME -t $IMAGE_NAME
+
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $APP_NAME
