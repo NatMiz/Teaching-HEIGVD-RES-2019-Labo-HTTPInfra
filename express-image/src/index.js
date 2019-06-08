e4d87a3c363efa6da9264cb46fd479a239a3b7e6
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-06-08 18:34:32 +0200

    Small http server with express

--- express-image/src/index.js
+++ express-image/src/index.js
@@ -1,7 +1,18 @@
-var Chance = require('chance');
+const Chance = require('chance');
+const Express = require('express');
 
+var app = Express();
 var chance = new Chance();
 
-for(i = 0; i < 10; i++){
-    console.log("Bonjour " + chance.name());
-}
+// console.log("Bonjour " + chance.name());
+app.get('/test', function(req, res){
+    res.send('Test is working\n');
+});
+
+app.get('/', function(req, res){
+    res.send('Hello RES\n');
+});
+
+app.listen(3000, function(){
+    console.log('Accepting HTTP requests on port 3000...');
+});
