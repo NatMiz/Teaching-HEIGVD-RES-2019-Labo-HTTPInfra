5f47696d3779a33319c08dd4fe9622fd612e8718
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-06-08 19:17:34 +0200

    Fonction that generates random string

--- express-image/src/index.js
+++ express-image/src/index.js
@@ -10,9 +10,29 @@ app.get('/test', function(req, res){
 });
 
 app.get('/', function(req, res){
-    res.send('Hello RES\n');
+    res.send(zootopiaRandom());
 });
 
 app.listen(3000, function(){
     console.log('Accepting HTTP requests on port 3000...');
 });
+
+function zootopiaRandom(){
+    // We'll return between 1 and 8 animals
+    let numberOfAnimals = chance.d8();
+    console.log('Number of animals: ' + numberOfAnimals + '\n');
+    // We'll returns an array of animals
+    let animals = [];
+
+    for(let i = 0; i < numberOfAnimals; ++i){
+        animals.push({
+            name: chance.suffix() + ' ' + chance.animal(),
+            profession: chance.profession(),
+            city: chance.city()
+        })
+    }
+
+    console.log(animals);
+
+    return animals;
+}
