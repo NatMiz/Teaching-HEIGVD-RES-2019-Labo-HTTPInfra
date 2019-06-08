0af28bd99773a1a96e483abf8ab42998a6105042
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-06-08 14:56:29 +0200

    Removed useless scripts

--- dockerClean.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/zsh
-
-# Stop container named apache-php
-docker stop apache-php
-
-# Remove container
-docker rm apache-php
--- dockerRun.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/zsh
-
-# Build docker image
-docker build -t my-php-app .
-
-# Run a container named apache-php
-docker run -d --name apache-php my-php-app 
