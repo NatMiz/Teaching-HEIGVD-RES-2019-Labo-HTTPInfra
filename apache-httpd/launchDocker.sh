f245f297be144591baf9338bd0c7c55e0386a7c8
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-06-08 14:52:59 +0200

    Script now returns container's IP address

--- apache-httpd/launchDocker.sh
+++ apache-httpd/launchDocker.sh
@@ -9,3 +9,5 @@ docker rm $APP_NAME
 docker build -t $IMAGE_NAME .
 
 docker run -d --name $APP_NAME -t $IMAGE_NAME
+
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $APP_NAME
