3d5a5f1beff09fa6deb7f73c7acd71611781f51b
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-06-08 19:24:00 +0200

    Express app works in container

--- express-image/launchDocker.sh
+++ express-image/launchDocker.sh
@@ -8,9 +8,9 @@ docker rm $APP_NAME
 
 docker build -t $IMAGE_NAME .
 
-#docker run -d --name $APP_NAME -t $IMAGE_NAME
+docker run -d --name $APP_NAME -t $IMAGE_NAME
 
 # For testing purpose
-docker run --name $APP_NAME -t $IMAGE_NAME
+#docker run --name $APP_NAME -t $IMAGE_NAME
 
 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $APP_NAME
